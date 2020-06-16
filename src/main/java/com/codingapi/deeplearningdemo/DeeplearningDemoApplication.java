0224a02a0c8a823565aa903ee7b176a5d7244e2f
wangliang <1991wangliang@gmail.com>
2020-06-16 09:12:01 +0800

    Initial commit

---
+++ src/main/java/com/codingapi/deeplearningdemo/DeeplearningDemoApplication.java
@@ -0,0 +1,13 @@
+package com.codingapi.deeplearningdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeeplearningDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeeplearningDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codingapi/deeplearningdemo/DeeplearningDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingapi.deeplearningdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeeplearningDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
